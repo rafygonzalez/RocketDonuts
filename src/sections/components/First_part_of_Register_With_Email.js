d9196c0bebd0a912320d4d40603cf3c25fdbc8f8
Rafael Gonzalez <rafygonzalez089@gmail.com>
2019-10-16 12:05:59 -0400

    git add .

---
+++ src/sections/components/First_part_of_Register_With_Email.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import PhotoCamera from '../../../assets/svg/photo-camera.svg';
+import TextInput from '../../screens/components/TextInput';
+import Button from '../../screens/components/button';
+import Estrellas from '../../../assets/svg/Estrellas_bw.svg';
+
+export default First_part_of_register = props => (
+  <SafeAreaView style={props.styles.area_container}>
+    <View style={props.styles.photo_container}>
+      <TouchableOpacity>
+        <View style={props.styles.photo_circle}>
+          <PhotoCamera width={32} height={32} />
+        </View>
+      </TouchableOpacity>
+      <Text style={props.styles.photo_text}>Sube una foto de perfil</Text>
+    </View>
+    <Text style={props.styles.description}>Información Personal</Text>
+    <ScrollView style={props.styles.info_container}>
+      <View style={{position: 'absolute'}}>
+        <Estrellas width={386} height={528} />
+      </View>
+      <View style={props.styles.box_container}>
+        <View style={props.styles.box}>
+          <TextInput
+            title="Nombre"
+            onChangeText={text => props.Global_OnChange(text, 'name')}
+            value={props.name}
+            autoCompleteType="username"
+          />
+        </View>
+
+        <View style={props.styles.box}>
+          <TextInput
+            title="Apellido"
+            onChangeText={text => props.Global_OnChange(text, 'lastname')}
+            value={props.lastname}
+            autoCompleteType="username"
+          />
+        </View>
+      </View>
+
+      <TextInput
+        title="Correo Electrónico"
+        onChangeText={text => props.Global_OnChange(text, 'email')}
+        value={props.email}
+        autoCompleteType="email"
+      />
+      <TextInput
+        title="Contraseña"
+        onChangeText={text => props.Global_OnChange(text, 'pass')}
+        value={props.pass}
+        autoCompleteType="password"
+        secureTextEntry={true}
+      />
+      <TextInput
+        title="Repite la Contraseña"
+        onChangeText={text => props.Global_OnChange(text, 'repass')}
+        value={props.repass}
+        autoCompleteType="password"
+        secureTextEntry={true}
+      />
+      <Button title="Siguiente (1/3)" button_style="primary" />
+    </ScrollView>
+  </SafeAreaView>
+);
