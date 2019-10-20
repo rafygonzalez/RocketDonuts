c64d61751aa62ce8adfaf72b8fe4284d3b140810
Rafael Gonzalez <rafygonzalez089@gmail.com>
2019-10-20 00:17:53 -0400

    Elaboracion de los productos en el home

--- src/screens/containers/Home.js
+++ src/screens/containers/Home.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {View, SafeAreaView, StyleSheet, ScrollView} from 'react-native';
 import Estrellas from '../../../assets/svg/Estrellas';
 import HeaderBanner from '../../sections/components/Header_Banner';
+import ProductBox from '../components/Product_Box';
+import Donut from '../../../assets/svg/Dona.svg';
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -14,9 +16,13 @@ class Home extends Component {
     return (
       <SafeAreaView style={styles.area_container}>
         <HeaderBanner withTitle />
-        <ScrollView style={styles.products_container}>
+        <ScrollView>
           <View style={styles.stars_container}>
             <Estrellas width={386} height={528} />
+          </View>
+          <View style={styles.products_container}>
+            <ProductBox item={<Donut />} item_name={'Donas'} />
+            <ProductBox item={<Donut />} item_name={'Donas'} />
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -29,7 +35,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#ECEDF2',
   },
   products_container: {
+    flexDirection: 'row',
     backgroundColor: '#ECEDF2',
+    marginVertical: 16,
+    marginHorizontal: 16,
   },
   stars_container: {
     position: 'absolute',
