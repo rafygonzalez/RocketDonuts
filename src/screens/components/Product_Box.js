cd107139a7d38e127baf0602d449d12dc4d13f1b
Rafael Gonzalez <rafygonzalez089@gmail.com>
2019-10-20 08:39:10 -0400

    Product Box adding responsive styles

--- src/screens/components/Product_Box.js
+++ src/screens/components/Product_Box.js
@@ -1,35 +1,68 @@
 import React from 'react';
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
-import {BoxShadow} from 'react-native-shadow';
-const shadowOpt = {
-  width: 164,
-  height: 180,
-  color: '#000',
-  border: 2,
-  radius: 6,
-  opacity: 0.2,
-  x: 0,
-  y: 1,
-  style: {
-    marginVertical: 8,
-    marginHorizontal: 8,
-  },
-};
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from 'react-native';
 
-export const Product_Box = props => {
-  return (
-    <BoxShadow setting={shadowOpt}>
+export class Product_Box_Large extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
       <TouchableOpacity
         onPress={props.onPress}
-        style={styles.item_box_container}>
+        style={styles.item_box_lg_container}>
+        <View style={styles.item_name_container}>
+          <Text style={styles.item_name_text}>{props.item_name}</Text>
+        </View>
+        <Image source={props.image} style={styles.image} />
+      </TouchableOpacity>
+    );
+  }
+}
+export class Product_Box extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {orientation: '', box_width: 0, box_heigth: 0};
+  }
+  getOrientation = () => {
+    var {width} = Dimensions.get('window');
+    var boxw_calc = (39.61 * width) / 100;
+    this.setState({
+      box_width: boxw_calc,
+      box_heigth: (108.54 * boxw_calc) / 100,
+    });
+  };
+  componentDidMount() {
+    this.getOrientation();
+    Dimensions.addEventListener('change', () => {
+      this.getOrientation();
+    });
+  }
+  render() {
+    const {props} = this;
+    const {box_width, box_heigth} = this.state;
+    return (
+      <TouchableOpacity
+        onPress={props.onPress}
+        style={[
+          styles.item_box_container,
+          {width: box_width, height: box_heigth},
+        ]}>
         <View style={styles.svg_container}>{props.item}</View>
         <View style={styles.item_name_container}>
           <Text style={styles.item_name_text}>{props.item_name}</Text>
         </View>
       </TouchableOpacity>
-    </BoxShadow>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   item_name_text: {
@@ -37,15 +70,26 @@ const styles = StyleSheet.create({
     fontSize: 30,
     textAlign: 'center',
   },
+  item_box_lg_container: {
+    borderRadius: 3,
+    borderWidth: 0.5,
+    borderColor: '#C7C8D6',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 8,
+    marginHorizontal: 8,
+    width: 342,
+    height: 138,
+  },
   item_box_container: {
     borderRadius: 3,
     borderWidth: 0.5,
     borderColor: '#C7C8D6',
-    width: 164,
-    height: 178,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#FFF',
+    marginVertical: 8,
+    marginHorizontal: 8,
   },
   svg_container: {
     width: 80,
--- src/screens/containers/Home.js
+++ src/screens/containers/Home.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
-import {View, SafeAreaView, StyleSheet, ScrollView} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+} from 'react-native';
 import Estrellas from '../../../assets/svg/Estrellas';
 import HeaderBanner from '../../sections/components/Header_Banner';
-import {Product_Box} from '../components/Product_Box';
+import {Product_Box, Product_Box_Large} from '../components/Product_Box';
 
 import Donut from '../../../assets/svg/Dona.svg';
 import Rosquilla from '../../../assets/svg/Rosquilla.svg';
@@ -10,16 +16,38 @@ import Rosquilla from '../../../assets/svg/Rosquilla.svg';
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {orientation: '', header_width: 0, header_heigth: 0};
   }
   static navigationOptions = {
     header: null,
   };
+  getOrientation = () => {
+    var {width} = Dimensions.get('window');
+
+    this.setState({
+      header_width: width,
+      header_heigth: (39.61 * width) / 100,
+    });
+    /* if (this.refs.rootView) {
+      if (Dimensions.get('window').width < Dimensions.get('window').height) {
+        this.setState({orientation: 'portrait'});
+      } else {
+        this.setState({orientation: 'landscape'});
+      }
+    }*/
+  };
+  componentDidMount() {
+    this.getOrientation();
+    Dimensions.addEventListener('change', () => {
+      this.getOrientation();
+    });
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.area_container}>
-        <HeaderBanner withTitle />
         <ScrollView>
+          <HeaderBanner withTitle />
           <View style={styles.stars_container}>
             <Estrellas width={386} height={528} />
           </View>
@@ -38,12 +66,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#ECEDF2',
   },
   products_container: {
+    flexWrap: 'wrap',
     flexDirection: 'row',
     backgroundColor: '#ECEDF2',
     marginVertical: 8,
     marginHorizontal: 8,
     justifyContent: 'center',
     alignItems: 'center',
+    alignContent: 'stretch',
   },
   stars_container: {
     position: 'absolute',
