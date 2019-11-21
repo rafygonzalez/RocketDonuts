4d344c70ccbf47a1c52a432803069209d04a0a53
Rafael Gonzalez <rafygonzalez089@gmail.com>
2019-11-21 16:52:43 -0400

    UI Building of ShowScreenshot and AttachScreenshot

--- src/screens/components/Complete_Order/ShowScreenshot.js
+++ src/screens/components/Complete_Order/ShowScreenshot.js
@@ -20,11 +20,21 @@ const ShowScreenshot = props => {
       onBack={props.onBack}
       buttons_component={<Buttons optionHandler={props.optionHandler} />}>
       <Text style={styles.title}>Vista previa</Text>
-      <Image source={props.avatarSource} style={styles.uploadAvatar} />
+      <View style={styles.uploadAvatar_container}>
+        <Image source={props.avatarSource} style={styles.uploadAvatar} />
+      </View>
     </Body>
   );
 };
 const styles = StyleSheet.create({
+  uploadAvatar_container: {
+    width: '100%',
+    height: hp('30%'),
+    backgroundColor: '#EDEEF4',
+    marginVertical: hp('2%'),
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   uploadAvatar: {
     width: '100%',
     height: hp('30%'),
@@ -35,7 +45,7 @@ const styles = StyleSheet.create({
     fontSize: wp('4%'),
     textAlign: 'center',
     color: '#313045',
-    marginVertical: hp('3%'),
+    marginVertical: hp('2%'),
   },
 });
 export default ShowScreenshot;
