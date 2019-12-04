864c4a57a300f3973aa92bca7882f8be33fc6811
Rafael Gonzalez <rafygonzalez089@gmail.com>
2019-12-04 11:54:58 -0400

    Display map, Get Current Position with GPS, move the camera with animation to the red point

--- src/screens/containers/Location.js
+++ src/screens/containers/Location.js
@@ -1,21 +1,15 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
-//import MapboxGL from '@react-native-mapbox-gl/maps';
 import Geolocation from '@react-native-community/geolocation';
-/*MapboxGL.setAccessToken(
-  'pk.eyJ1IjoicmFmeWdvbnphbGV6MDg5IiwiYSI6ImNqejhlMDVjODFrZ2kzaW1qbDV1bnh2cHoifQ.T3vurNbpjgq_aRL7QjuvpQ',
-);*/
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   container: {
-    height: 300,
-    width: 300,
+    height: '60%',
     backgroundColor: 'tomato',
   },
   map: {
@@ -28,36 +22,84 @@ export default class Location extends Component {
     super(props);
     this.state = {
       location: null,
+      mapisReady: false,
     };
+    this._map = null;
+    this.animateCamera = this.animateCamera.bind(this);
+    this.onMapReady = this.onMapReady.bind(this);
   }
 
-  componentDidMount() {}
+  async onMapReady() {
+    try {
+      const position = await this.findCoordinates();
+      setTimeout(async () => await this.animateCamera(position), 1500);
+      this.setState({location: position});
+    } catch (e) {}
+  }
+  animateCamera(location) {
+    return new Promise(resolve => {
+      this._map.animateCamera(
+        {
+          center: {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          },
+          zoom: 15,
+        },
+        15000,
+      );
+      resolve(true);
+    });
+  }
   findCoordinates = () => {
-    Geolocation.getCurrentPosition(
-      position => {
-        const location = JSON.stringify(position);
-        console.warn(location);
-        this.setState({location});
-      },
-      error => Alert.alert(error.message),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
-    );
+    return new Promise((resolve, reject) => {
+      Geolocation.getCurrentPosition(
+        position => {
+          resolve(position);
+        },
+        error => reject(error.message),
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+      );
+    });
   };
   render() {
+    const {location} = this.state;
     return (
       <View style={styles.page}>
         <View style={styles.container}>
           <MapView
+            ref={map => {
+              this._map = map;
+            }}
             provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            onMapReady={() => this.onMapReady()}
             style={styles.map}
             region={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.0121,
-            }}></MapView>
+              latitude: 10.1673862,
+              longitude: -64.677611,
+              latitudeDelta: 0.2,
+              longitudeDelta: 0.2,
+            }}>
+            {location !== null && (
+              <Marker
+                coordinate={{
+                  latitude: location.coords.latitude,
+                  longitude: location.coords.longitude,
+                }}
+                title={'¡Estas aqui!'}
+                description={
+                  'Esta es tu ubicación GPS, guardala en tu directorio.'
+                }
+              />
+            )}
+          </MapView>
+        </View>
+        <View>
+          <Text>Tu Dirección:</Text>
+          <Text>Los Apamates, Barcelona 6001, Anzoategui, Vnezuelaa</Text>
         </View>
       </View>
     );
   }
 }
+/*        */
